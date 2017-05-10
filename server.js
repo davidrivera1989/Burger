cea122e6ef54785b8cb65f3ccd492a834cbb6d77
David Rivera <David@calvisitor-10-105-184-45.calvisitor.1918.berkeley.edu>
2017-05-09 18:53:49 -0700

    BurgerApp

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
